3faff5019591d526617a0a1b3fa450ae114fa8e9
Smoil <95854119+Smoil1536@users.noreply.github.com>
2024-10-01 23:41:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,107 @@
+const API_KEY = "$2a$10$Rc9zVq.18U/l6zYP.ucXnOEY1nxq7z3M07sd7WPea2bKoW2l561Tq";
+const ROOT_URL = "https://api.jsonbin.io/v3/b";
+
+class NoteHandler {
+    constructor(id) {
+        this.id = id;
+    }
+    async post(data) {
+        try {
+            let response = await fetch(ROOT_URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Master-Key": API_KEY,
+                    "X-Bin-Private": false,
+                },
+                body: data,
+            });
+            let result = await response.json();
+            console.log(result);
+
+            return result["metadata"]["id"];
+        } catch (err) {
+            throw new console.error(err);
+        }
+    }
+
+    async get(binID) {
+        try {
+            let response = await fetch(ROOT_URL + "/" + binID, {
+                method: "GET",
+                headers: {
+                    "X-Master-Key": API_KEY,
+                },
+            });
+
+            let result = await response.json();
+            console.log(result);
+            return result;
+        } catch (err) {
+            throw new console.error(err);
+        }
+    }
+}
+
+const params = new URLSearchParams(window.location.href);
+const shareID = params.get("share");
+
+if (shareID !== null) {
+    const receive = new NoteHandler("anything for now");
+    
+    receive.get(shareID)
+        .then(result => {
+            const note = document.querySelector(".note");
+            const name = document.querySelector(".sender-name");
+
+            name.innerHTML = result["result"]["name"];
+            note.value = result["result"]["note"];
+        });
+}
+
+async function share() {
+    // const ID = generateID();
+    const send = new NoteHandler("anything for now");
+    const data = createData();
+
+    const ID = await send.post(data);
+    const param = new URLSearchParams();
+
+    param.append("share", ID);
+    console.log(window.location.href + "?" + param.toString());
+}
+function createData() {
+    const note = document.querySelector(".note").value;
+    const name = document.querySelector(".sender-name").innerHTML;
+    
+    const data = {
+        "sender": name,
+        "note": note,
+    };
+    
+    return JSON.stringify(data);
+}
+// function generateID() {
+//     const d = new Date();
+//     let time = d.getTime();
+//     let id = String(Math.floor(Math.random() * 1000000)) + String(time);
+
+//     return id;
+// }
+// async function postNote() {
+//     try {
+//         let response = await fetch(ROOT_URL, {
+//             method: "POST",
+            
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "X-Master-Key": API_KEY
+//             },
+//             body: {
+//                 "name": "anythin" 
+//             },
+//         });
+//     } catch (err) {
+//         throw new console.error(err);
+//     }
+// }
